2b25188fa68d8bb762d4a62693b7bb12218d4c07
Carrie Kraft <carriekarft@gmail.com>
2023-04-28 16:04:49 -0700

    commit tree 6c69ad97b33771f3bd4b7d1443b1b2972526ca92

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet "Carrie"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+
+def add num1, num2
+    num1 + num2
+end
+
+result = add 3, 4
+puts result
+
+def halve num
+    if num.class != Integer
+        return nil
+    end
+    num / 2
+end
+
+halve_result = halve 66
+puts halve_result
